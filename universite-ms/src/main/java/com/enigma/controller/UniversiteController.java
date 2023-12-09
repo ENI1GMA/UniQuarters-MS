a14548a7de326e5bad48198483c22d832509f080
Med Yacine Khouini <khouini.medyacine@gmail.com>
2023-12-09 17:46:11 +0100

    universite

--- universite-ms/src/main/java/com/enigma/controller/UniversiteController.java
+++ universite-ms/src/main/java/com/enigma/controller/UniversiteController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
+@CrossOrigin(origins = "http://localhost:3081", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,RequestMethod.DELETE})
 @RestController
 @RequestMapping("/universities")
 @AllArgsConstructor
@@ -42,6 +43,58 @@ public class UniversiteController {
         }
         return new ResponseEntity<>(apiResponse, apiResponse._getHttpStatus());
     }
+    @PostMapping("")
+    public ResponseEntity<ApiResponse>  addUni(@RequestBody Universite uni) {
+        System.out.println("Received POST request for adding university: " + uni);
+
+        ApiResponse apiResponse = new ApiResponse();
+        try {
+
+            apiResponse.setResponse(org.springframework.http.HttpStatus.CREATED, "University added");
+
+            apiResponse.addData("universite", universiteRepo.save(uni));
+        } catch (Exception e) {
+            apiResponse.setResponse(org.springframework.http.HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+        return new ResponseEntity<>(apiResponse, apiResponse._getHttpStatus());
+
+
+    }
+    @PostMapping("/testpost")
+    public ResponseEntity<String> testPost() {
+        return ResponseEntity.ok("Test POST request successful");
+    }
+    @PutMapping("/{idUni}")
+    public ResponseEntity<ApiResponse> updateFoyer(@RequestBody Universite uni, @PathVariable long idUni) {
+        ApiResponse apiResponse = new ApiResponse();
+        try {
+            Universite u = universiteRepo.findById(idUni).orElse(null);
+
+            if (u == null) {
+                apiResponse.setResponse(org.springframework.http.HttpStatus.NOT_FOUND, "University not found");
+                return new ResponseEntity<>(apiResponse, apiResponse._getHttpStatus());
+            }
+
+            u.setNom(uni.getNom());
+            u.setAdresse(uni.getAdresse());
+            u.setImage(uni.getImage());
+            Universite updatedUni = universiteRepo.save(u);
+
+            apiResponse.setResponse(org.springframework.http.HttpStatus.CREATED, "University updated");
+            apiResponse.addData("university", updatedUni);
+        } catch (Exception e) {
+            apiResponse.setResponse(org.springframework.http.HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+
+        return new ResponseEntity<>(apiResponse, apiResponse._getHttpStatus());
+    }
+
+
+    @DeleteMapping  ("/{idUni}")
+    public void delete(@PathVariable long idUni) {
+        universiteRepo.deleteById(idUni);
+    }
+
 
 }
 
