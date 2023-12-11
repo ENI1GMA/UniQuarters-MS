81a778e8c9aa99d532bb2cb84bc4558fe8dd1452
Med Yacine Khouini <khouini.medyacine@gmail.com>
2023-12-11 01:50:24 +0100

    reservation working with gateway eureka

--- reservation-ms-expressjs/config/eurekaClient.js
+++ reservation-ms-expressjs/config/eurekaClient.js
@@ -7,19 +7,18 @@ const eurekaConfig = {
     app: 'RESERVATION-SERVICE-EXPRESSJS', // Replace with your service name
     hostName: 'localhost',
     ipAddr: '127.0.0.1',
+    statusPageUrl: 'http://localhost:3000/info',
     port: {
       $: process.env.PORT,
       '@enabled': 'true',
     },
-    vipAddress: 'your-service-name', // Replace with your service name
+    vipAddress: 'RESERVATION-SERVICE-EXPRESSJS', // Replace with your service name
     dataCenterInfo: {
       '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
       name: 'MyOwn',
     },
   },
   eureka: {
-    // host: 'serviceregistry',
-    // host: 'localhost',
     host: process.env.EUREKA_CLIENT_HOST,
     port: 8761,
     servicePath: '/eureka/apps/',
