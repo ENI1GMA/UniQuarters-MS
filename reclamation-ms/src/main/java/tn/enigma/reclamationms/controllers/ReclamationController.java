cd142f8f57866a130b1f817c79b3545e7b1cd3e7
Med Yacine Khouini <khouini.medyacine@gmail.com>
2023-12-12 22:44:18 +0100

    reclamation integred & works

--- reclamation-ms/src/main/java/tn/enigma/reclamationms/controllers/ReclamationController.java
+++ reclamation-ms/src/main/java/tn/enigma/reclamationms/controllers/ReclamationController.java
@@ -10,7 +10,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @CrossOrigin(origins = "http://localhost:4200/", allowedHeaders = "*")
-@RequestMapping("reclamation")
+@RequestMapping("reclamations")
 public class ReclamationController {
     private final ReclamationService service;
 
