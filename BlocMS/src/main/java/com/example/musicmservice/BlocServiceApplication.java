8fa9e31ec87ed0dbf1ce4ff19726bc280d42404e
MoezBenHassen <moezbh.mbh@gmail.com>
2023-12-12 15:34:54 +0100

    rn

--- BlocMS/src/main/java/com/example/musicmservice/BlocServiceApplication.java
+++ BlocMS/src/main/java/com/example/musicmservice/BlocServiceApplication.java
@@ -1,20 +1,15 @@
 package com.example.musicmservice;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-
-import java.util.HashSet;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class MusicMServiceApplication {
+public class BlocServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MusicMServiceApplication.class, args);
+        SpringApplication.run(BlocServiceApplication.class, args);
     }
 
 
