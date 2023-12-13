f045fdcdf753f7433bc004feb7cb899f19b1a1f0
Nassim BenSalah <72417267+BSnassim@users.noreply.github.com>
2023-12-13 23:41:30 +0100

    Gestion reclamation (#14)
    * changed to new keycloak login
    
    * added reclamation to angular

---
+++ UniQuarters-Angular/src/app/features/reclamation/reclamation-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ListReclamationComponent } from './list-reclamation/list-reclamation.component';
+
+const routes: Routes = [
+  {path:'',component:ListReclamationComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ReclamationRoutingModule { }
