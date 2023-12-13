8c5a75b9ec8e5215b25a4b316e679205296452eb
Med Yacine Khouini <khouini.medyacine@gmail.com>
2023-12-14 00:56:28 +0100

    fix resa

--- reservation-ms-expressjs/routes/reservation.js
+++ reservation-ms-expressjs/routes/reservation.js
@@ -5,7 +5,7 @@ const ReservationController = require('../controllers/reservation.js');
 
 router.get('/', ReservationController.getAllReservations);
 router.get('/ChambresReservations', ReservationController.getChambresReservationsStatistiques);
-router.get('/:id', ReservationController.getReservation);
+router.get('/resa/:id', ReservationController.getReservation);
 router.get('/etudiant/:id', ReservationController.getReservationByEtudiant);
 router.patch('/valider/:idReservation', ReservationController.validerReservation);
 router.post('/:idChambre/:idEtudiant', ReservationController.createReservation);
